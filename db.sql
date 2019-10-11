34c87a79740caa41863d4617bc4b2098741e0ca2
jefferson.espana <jefferson.espana@estratek.com.co>
2019-10-11 11:31:19 -0500

    crud students

---
+++ db.sql
@@ -0,0 +1,25 @@
+create table course
+(
+    uuid_course     text not null
+        constraint course_pkey
+            primary key,
+    title       text,
+    teacher     text,
+    description text,
+    topic       text,
+    created_at  timestamp with time zone,
+    updated_at  timestamp with time zone
+);
+
+
+create table student
+(
+    uuid_student     text not null
+        constraint student_pkey
+            primary key,
+    name       text,
+    email     text,
+    created_at  timestamp with time zone,
+    updated_at  timestamp with time zone
+);
+
